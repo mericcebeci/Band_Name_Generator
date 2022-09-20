bcb6beca9551d761c7c25a80ff6a3fefe1882987
Meriç Cebeci <mericcebeci@gmail.com>
2022-09-20 13:50:27 +0300

    adding a new line

--- Band_Name_Generator/main.py
+++ Band_Name_Generator/main.py
@@ -17,6 +17,7 @@
 
 
 print("Welcome to the Band Name Generator.")
+print("Form Meriç Cebeci :) \n")
 street = input("What's name of the city you grew up in?\n")
 pet = input("What's your pet's name?\n")
-print("Your band name could be " + street + " " + pet)
+print("Your band name could be \n" + street + " " + pet)
